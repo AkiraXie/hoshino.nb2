3ddeed46d5135dfd6e71ab62498536a9179ff368
AkiraXie <l997460364@outlook.com>
2021-02-13 01:13:05 +0800

    :children_crossing: RSS支持图片内容的输出； high_eq支持换行符。 :bug: 修复aiohttpx不能读取图像的bug

--- hoshino/modules/entertainment/high_eq.py
+++ hoshino/modules/entertainment/high_eq.py
@@ -2,12 +2,13 @@
 Author: AkiraXie
 Date: 2021-02-12 01:25:05
 LastEditors: AkiraXie
-LastEditTime: 2021-02-12 01:36:02
+LastEditTime: 2021-02-12 22:35:58
 Description: 
 Github: http://github.com/AkiraXie/
 '''
 from hoshino import Service, R,Bot,MessageSegment
 from hoshino.util import ImageFont, Image, ImageDraw,pic2b64
+from re import S
 from hoshino.typing import T_State
 path = R.img('high_eq_image.png')
 sv = Service('high-eq', enable_on_default=False)
@@ -30,7 +31,7 @@ def draw_text(img_pil: Image.Image, text: str, offset_x: int):
     draw.text((x + offset_x, 360), text, font=font, fill=(255, 255, 255, 255))
 
 
-loweq=sv.on_regex(r'低情商(.{1,15})高情商(.{1,15})',only_group=False)
+loweq=sv.on_regex(r'低情商(.{1,15})高情商(.{1,15})',flags=S,only_group=False)
 @loweq.handle()
 async def _(bot:Bot,state:T_State):
     match=state['match']
