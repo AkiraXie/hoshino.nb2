5bcadafb45e810d7e63152b040dede6760c0534b
AkiraXie <l997460364@outlook.com>
2021-05-16 18:09:17 +0800

    ✨ 新增饮茶

---
+++ hoshino/modules/entertainment/yincha.py
@@ -0,0 +1,25 @@
+'''
+Author: AkiraXie
+LastEditTime: 2021-05-16 18:07:32
+LastEditors: AkiraXie
+GitHub: https://github.com/AkiraXie
+'''
+from hoshino import Service, R, permission, scheduled_job, MessageSegment, Bot
+video = R+'video/yincha.mp4'
+videomsg = MessageSegment.video(f'file:///{video}')
+
+sv = Service('yincha', manage_perm=permission.SUPERUSER,
+             visible=False, enable_on_default=False)
+
+
+@scheduled_job('cron', hour='15', minute='00', jitter=20, id='yincha')
+async def _():
+    await sv.broadcast(videomsg, 'yincha', 0.2)
+    
+    
+yinchacmd = sv.on_fullmatch(keywords={'饮茶','yincha'})
+
+
+@yinchacmd
+async def _(bot: Bot):
+    await yinchacmd.send(videomsg)
