2dea9f58e6c96e63a17eb262bc3966b50841adf3
AkiraXie <l997460364@outlook.com>
2024-03-07 18:50:44 +0800

    ✨ Add bilibili json msg handler

--- hoshino/modules/tools/bilibili/__init__.py
+++ hoshino/modules/tools/bilibili/__init__.py
@@ -10,10 +10,38 @@ import asyncio
 from hoshino import Service, Bot, Event, Message
 from hoshino.typing import T_State
 from .data import get_bvid, get_resp
-
+from json import loads
 sv = Service("bilibili")
 bl = sv.on_regex(r"b23.tv\\?/([A-Za-z0-9]{6,7})", normal=False, full_match=False)
 bv = sv.on_regex(r"BV[A-Za-z0-9]{10}", normal=False, full_match=False)
+
+async def check_bvjson(ev:Event,state:T_State) -> bool:
+    for s in ev.get_message():
+        if s.type == "json":
+            data = loads(s.data.get("data","{}"))
+            meta = data.get("meta")
+            if meta and (c:=meta.get("detail_1")):
+                if q:=c.get("qqdocurl"):
+                    state["bvurl"] = q
+                    return True
+    return False
+
+bvjson = sv.on_message(rule=check_bvjson)
+
+
+@bvjson.handle()
+async def _(state: T_State):
+    if not (url:=state.get("bvurl")):
+        return
+    bvid = await get_bvid(url)
+    if not bvid:
+        return
+    msg = await get_resp(bvid)
+    if not msg:
+        return
+    await bvjson.send("检测到b站视频，正在解析~")
+    await asyncio.sleep(0.3)
+    await bvjson.finish(msg)
 
 
 @bl
