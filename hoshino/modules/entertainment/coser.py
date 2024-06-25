eeb0a8e5cf080c3df83b892f8eb2d42cf0c6c06f
AkiraXie <l997460364@outlook.com>
2024-06-25 17:59:05 +0800

    add coser

---
+++ hoshino/modules/entertainment/coser.py
@@ -0,0 +1,17 @@
+from hoshino import Service, Event, MessageSegment
+from hoshino.util import Cooldown, aiohttpx
+
+sv = Service("coser", visible=False, enable_on_default=False)
+
+
+
+
+m = sv.on_command("coser",only_to_me=True)
+
+
+@m
+async def _( _=Cooldown(10,"冲太快了")):
+    res = await aiohttpx.get("https://api.suyanw.cn/api/cos.php?type=json")
+    data = res.json["text"]
+    await m.send(MessageSegment.image(data))
+ 
