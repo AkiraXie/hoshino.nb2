63fa5f31b897eb8319be9fba6b4ce366703cb6a5
AkiraXie <l997460364@outlook.com>
2024-07-21 18:25:38 +0800

    ⚡️ add lagrange stats

--- hoshino/modules/develop/server_info.py
+++ hoshino/modules/develop/server_info.py
@@ -12,12 +12,13 @@ import time
 from hoshino import sucmd, Bot, driver
 from hoshino.util import send_to_superuser
 from asyncio import all_tasks
-from datetime import datetime,timedelta
+from datetime import datetime,timedelta,UTC
 import psutil
 showcmd = sucmd("info", aliases={"serverinfo", "stat"})
 p = psutil.Process()
 p1 :psutil.Process = None
-epoch = datetime.utcfromtimestamp(0)
+p2 :psutil.Process = None
+epoch = datetime.fromtimestamp(0,UTC)
 def refresh_gocq_process():
     global p1
     _p = None
@@ -27,16 +28,34 @@ def refresh_gocq_process():
             break     
     p1 = _p
 
+def refresh_lag_process():
+    global p2
+    _p = None
+    for ps in psutil.process_iter():
+        if  "Lagrange" in ps.name() :
+            _p = psutil.Process(ps.pid)
+            break     
+    p2 = _p
+
+
 def get_gocq_process():
     if p1:
         return p1
     else:
         refresh_gocq_process()
         return p1
-    
+
+
+def get_lag_process():
+    if p2:
+        return p2
+    else:
+        refresh_lag_process()
+        return p2
+
 def get_stat():
     tasks = all_tasks()
-    cpu_p = p.cpu_percent()
+    cpu_p = p.cpu_percent(1)
     mem = p.memory_full_info()
     memu = mem.uss / 1024.0 / 1024.0
     disk = psutil.disk_usage("/")
@@ -49,19 +68,22 @@ def get_stat():
     msg = [
         f"服务CPU使用: {cpu_p}%",
         f"服务内存使用: {memu:.2f}MB",
-        f"磁盘使用: {dp}%  {du:.2f}GB/{dt:.2f}GB",
         f"服务协程数量: {len(tasks)}",
-        f"服务运行时间: {datetime.utcfromtimestamp(live_time)-epoch}",
+        f"服务运行时间: {datetime.fromtimestamp(live_time,UTC)-epoch}",
+        f"磁盘使用: {dp}%  {du:.2f}GB/{dt:.2f}GB",
     ]
     pp = get_gocq_process()
-    if not pp:
+    pl = get_lag_process()
+    if not pp and not pl:
         return "\n".join(msg)
-    cpu_p1 = pp.cpu_percent()
-    mem1 = pp.memory_full_info().uss / 1024.0 / 1024.0
-    gocq_live_time = time.time() - pp.create_time()
-    msg.extend([f"go-cqhttp CPU使用: {cpu_p1}%", 
-                f"go-cqhttp 内存使用: {mem1:.2f}MB",
-                f"go-cqhttp 运行时间: {datetime.utcfromtimestamp(gocq_live_time)-epoch}"])
+    name = "go-cqhttp" if pp else "Lagrange"
+    p = pp if pp else pl
+    cpu_p1 = p.cpu_percent(1)
+    mem1 = p.memory_full_info().uss / 1024.0 / 1024.0
+    p_live_time = time.time() - p.create_time()
+    msg.extend([f"{name} CPU使用: {cpu_p1}%", 
+                f"{name} 内存使用: {mem1:.2f}MB",
+                f"{name} 运行时间: {datetime.fromtimestamp(p_live_time,UTC)-epoch}"])
     return "\n".join(msg)
 
 
@@ -73,5 +95,5 @@ async def _():
 @driver.on_bot_connect
 async def _(bot: Bot):
     refresh_gocq_process()       
-    #await send_to_superuser(bot, get_stat())
+    await send_to_superuser(bot, get_stat())
 
