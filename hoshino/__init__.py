eb11c0cffa2309a7c036f834734db8ca6c9a3db5
AkiraXie <l997460364@outlook.com>
2025-05-11 08:58:32 +0800

    fix

--- hoshino/__init__.py
+++ hoshino/__init__.py
@@ -190,8 +190,8 @@ e.g：
 R: Final[RHelper] = RHelper()
 
 
-on_startup = driver.on_startup
-on_shutdown = driver.on_shutdown
+on_startup = driver.on_bot_connect
+on_shutdown = driver.on_bot_disconnect
 
 from .permission import SUPERUSER
 from .schedule import scheduled_job, add_job
