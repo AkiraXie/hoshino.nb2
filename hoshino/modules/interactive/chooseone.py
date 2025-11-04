30298850a94a8e9c9ce2105de66291920a60e114
AkiraXie <l997460364@outlook.com>
2025-11-04 11:06:21 +0800

    ⚡️ use system random to chooseone

--- hoshino/modules/interactive/chooseone.py
+++ hoshino/modules/interactive/chooseone.py
@@ -7,6 +7,7 @@ co = sv.on_command("选择", only_group=False, priority=2)
 
 @co.handle()
 async def _(bot: Bot, event: Event):
+    rng = random.SystemRandom()
     msg = str(event.get_message())
     msg = msg.split("还是")
     if len(msg) == 1:
@@ -17,9 +18,9 @@ async def _(bot: Bot, event: Event):
     msgs = ["您的选项是:"]
     idchoices = list(f"{i + 1}. {choice}" for i, choice in enumerate(choices))
     msgs.extend(idchoices)
-    if random.randint(0, 1000) <= 76:
+    if rng.randint(0, 1000) <= 76:
         msgs.append("建议您选择: “我全都要”")
     else:
-        final = random.choice(choices)
+        final = rng.choice(choices)
         msgs.append(f"建议您选择: {final}")
     await co.finish(Message("\n".join(msgs)), call_header=True)
