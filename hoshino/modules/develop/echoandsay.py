e89b036f5ea87833b35f5694395884d10cb30264
AkiraXie <l997460364@outlook.com>
2026-07-01 06:43:17 +0800

    Migrate echoandsay to sv.on_alconna, remove sucmd dependency

--- hoshino/modules/develop/echoandsay.py
+++ hoshino/modules/develop/echoandsay.py
@@ -1,20 +1,17 @@
 from html import unescape
 
-from hoshino.util import sucmd
 from hoshino.service import Service
 from hoshino.permission import SUPERUSER
-from hoshino.platform import Alconna, MsgId, UniMessage, UniMsg
+from hoshino.platform import Alconna, Args, MsgId, UniMessage, UniMsg
+
+sv = Service("echoandsay", manage_perm=SUPERUSER, enable_on_default=True)
 
 
-async def handle_echo(msg: UniMsg):
+@sv.on_alconna(Alconna("echo", Args["text", str]))
+async def _(msg: UniMsg):
     await UniMessage.text(unescape(str(msg))).send()
 
 
-async def handle_reply(msg: UniMsg, message_id: MsgId):
+@sv.on_alconna(Alconna("say", Args["text", str]))
+async def _(msg: UniMsg, message_id: MsgId):
     await UniMessage.text(unescape(str(msg))).send(reply_to=message_id)
-
-
-echo = sucmd("echo")
-echo.handle()(handle_echo)
-sv = Service("say", manage_perm=SUPERUSER, enable_on_default=False)
-sv.on_alconna(Alconna("say"), only_group=False)(handle_echo)
