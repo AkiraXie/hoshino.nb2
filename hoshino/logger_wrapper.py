cb2ad141bf9f73971798d61740144c6227024ec9
AkiraXie <l997460364@outlook.com>
2026-07-01 20:14:29 +0800

    Remove dead compat shims: rule.py, logger_wrapper.py (zero callers)

--- hoshino/logger_wrapper.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility shim for hoshino.core.logger_wrapper."""
-
-from hoshino.core.logger_wrapper import LoggerWrapper as LoggerWrapper
-
-__all__ = ["LoggerWrapper"]
--- hoshino/rule.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility shim for hoshino.core.rule."""
-
-from hoshino.core.rule import fullmatch as fullmatch
-from hoshino.core.rule import keyword as keyword
-from hoshino.core.rule import regex as regex
-from nonebot.rule import Rule as Rule
-
-__all__ = ["Rule", "fullmatch", "keyword", "regex"]
