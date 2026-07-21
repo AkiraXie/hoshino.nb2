90bac5a2bf5480a69aae171e4996aad339960afb
AkiraXie <l997460364@outlook.com>
2026-07-21 19:23:48 +0800

    Ignore something

--- hoshino/modules/entertainment/coser.py
+++ hoshino/modules/entertainment/coser.py
@@ -1,15 +1,15 @@
-from hoshino.service import Service
-from hoshino.command import Alconna, UniMessage
-from hoshino.util import Cooldown, aiohttpx
+# from hoshino.service import Service
+# from hoshino.command import Alconna, UniMessage
+# from hoshino.util import Cooldown, aiohttpx
 
-sv = Service("coser", visible=False, enable_on_default=False)
-
-
-m = sv.on_alconna(Alconna("coser"), only_to_me=True)
+# sv = Service("coser", visible=False, enable_on_default=False)
 
 
-@m.handle()
-async def _(_=Cooldown(10, "冲太快了")):
-    res = await aiohttpx.get("https://api.suyanw.cn/api/cos.php?type=json")
-    data = res.json["text"]
-    await UniMessage.image(url=data).send()
+# m = sv.on_alconna(Alconna("coser"), only_to_me=True)
+
+
+# @m.handle()
+# async def _(_=Cooldown(10, "冲太快了")):
+#     res = await aiohttpx.get("https://api.suyanw.cn/api/cos.php?type=json")
+#     data = res.json["text"]
+#     await UniMessage.image(url=data).send()
