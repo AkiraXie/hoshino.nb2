7e69e1c4c44ef7e295ad438bdda5f4906916e8bb
AkiraXie <l997460364@outlook.com>
2026-09-04 15:40:39 +0800

    fix(say): no return to blank text

--- hoshino/modules/develop/echoandsay.py
+++ hoshino/modules/develop/echoandsay.py
@@ -5,15 +5,19 @@ from hoshino.service import Service
 
 sv = Service("echoandsay", manage_perm=SUPERUSER, enable_on_default=True)
 
-echo = sv.on_command("echo")
-say = sv.on_command("say")
+echo = sv.on_command(name="echo",permission=SUPERUSER)
+say = sv.on_command(name="say")
 
 
 @echo.handle()
 async def _(text: str = ParamText()):
+    if not text:
+        return
     await UniMessage.text(text).send()
 
 
 @say.handle()
 async def _(message_id: MsgId, text: str = ParamText()):
+    if not text:
+        return
     await UniMessage.text(text).send(reply_to=message_id)
